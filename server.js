58e830e95e4f23303bdff28465dee54ab087bc17
akhl <akhilmadavana@gmail.com>
2016-03-03 15:55:40 +0200

    save changes

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+
+});
+app.get('/pages', function (req, res) {
+  res.json([
+      {
+        title: 'main title',
+        content: 'Lorem ipsum 1'
+      },
+      {
+        title: 'about title',
+        content: 'Lorem ipsum 2'
+      },
+      {
+        title: 'contact title',
+        content: 'Lorem ipsum 3'
+      }
+    ]);
+  });
+
+app.listen(3000, function () {
+ consoleapp.get('/', function (req, res) {
+
+ });.log('working');
+});
